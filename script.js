b2ae76e099c6bddbe2181c99fdeb8f763ffa7e13
Kuro Hsu <kurotanshi@gmail.com>
2020-11-07 22:02:18 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function printStars(count) {
+  let str = '';
+  let i, j;
+
+  for(i = 1; i <= count; i++) {
+    for(j = 1; j <= i; j++) {
+      str += '*';
+    }
+    str += '\n';
+  }
+  console.log(str);
+}
+
+// 請參考 printStars，
+// 並完成 printStars2，使其執行時結果如圖： https://imgur.com/56BptSu
+function printStars2(count) {
+  // todo...
+  
+}
